0447bcffa3a540da86bc35f902a8d8788201674d
spacedevin <git@arzynik.com>
2026-03-20 19:18:52 -0700

    cleanup

--- wasi_build/wasi_out/src/main.rs
+++
@@ -1,8 +0,0 @@
-
-fn main() {
-    let chunk = include_bytes!("../chunk.bin");
-    if let Err(e) = tish_wasm_runtime::run_wasi(chunk) {
-        eprintln!("Runtime error: {}", e);
-        std::process::exit(1);
-    }
-}
